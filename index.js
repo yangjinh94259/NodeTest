36e0e56b8376248e178c35c3e883313ef5ea39cc
yangjinh94259 <121842592+yangjinh94259@users.noreply.github.com>
2023-07-10 20:03:34 +0900

    node test

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/user/:id', (req, res) => {
+    // const q = req.params
+    // console.log(q.id)
+    const q = req.query
+    console.log(q.q)
+    console.log(q.name)
+
+    res.json({'userId ' : q.name})
+  })
+
+app.get('/sound/:name', (req, res) => {
+    const {name} = req.params
+
+    if(name == 'dog'){
+        res.json({'sound' : '멍멍'})
+    } else if(name == 'cat'){
+        res.json({'sound' : '야옹'})
+    } else if(name == 'pig'){
+        res.json({'sound' : '꿀꿀'})
+    } else{
+        res.json({'sound' : '알수없음'})
+    }
+
+    console.log(name)
+
+    res.json({'sound ' : '야용'})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
